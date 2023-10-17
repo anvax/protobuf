8300ede8bc20bbc01589e8d10370c42633b21ea1
anvax <andreykhudobin23@gmail.com>
2023-10-17 12:00:51 +0300

    parser

--- 102-cpp-protobuf-template-protobuf/102-cpp-protobuf-template-protobuf/src/protobuf_parser/DelimitedMessagesStreamParser.hpp
+++ 102-cpp-protobuf-template-protobuf/102-cpp-protobuf-template-protobuf/src/protobuf_parser/DelimitedMessagesStreamParser.hpp
@@ -14,11 +14,38 @@ class DelimitedMessagesStreamParser
 {
 public:
     typedef std::shared_ptr<const MessageType> PointerToConstValue;
-
+    typedef std::list<PointerToConstValue> ParsedMsgsList;
     std::list<PointerToConstValue> parse(const std::string& data);
 
 private:
     std::vector<char> m_buffer;
+    void addToBuffer(std::string &data){
+        m_buffer.insert(m_buffer.cend(), data.cbegin(), data.cend());
+    };
+    void deleteFromBuffer(size_t size){
+        m_buffer.erase(m_buffer.cbegin(), m_buffer.cbegin() + size);
+    };
+
 };
+template<typename MessageType>
+typename DelimitedMessagesStreamParser<MessageType>::ParsedMsgsList DelimitedMessagesStreamParser<MessageType>::parse(
+        const std::string &data) {
+    addToBuffer(data);
+
+    std::list<PointerToConstValue> list;
+    size_t usedBytes = -1;
+
+    while (usedBytes != 0)
+    {
+        auto parsedMsg = parseDelimited<MessageType>(static_cast<const void*>(&m_buffer),m_buffer.size(),&usedBytes);
+        if (parsedMsg != nullptr){
+            list.push_back(parsedMsg);
+            deleteFromBuffer(usedBytes);
+        }
+    }
+
+    return list;
+}
+
 
 #endif /* SRC_PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_HPP_ */
--- 102-cpp-protobuf-template-protobuf/102-cpp-protobuf-template-protobuf/src/protobuf_parser/helpers.hpp
+++ 102-cpp-protobuf-template-protobuf/102-cpp-protobuf-template-protobuf/src/protobuf_parser/helpers.hpp
@@ -4,6 +4,8 @@
  *  Created on: 2 Feb 2023
  *      Author: sia
  */
+#include <boost/make_shared.hpp>
+#include <google/protobuf/io/coded_stream.h>
 
 #ifndef SRC_PROTOBUF_PARSER_HELPERS_H_
 #define SRC_PROTOBUF_PARSER_HELPERS_H_
@@ -17,7 +19,15 @@ typedef std::vector<char> Data;
 typedef std::shared_ptr<const Data> PointerToConstData;
 
 template <typename Message> PointerToConstData serializeDelimited(const Message& msg) {
-    return nullptr;
+    const size_t messageSize = PROTOBUF_MESSAGE_BYTE_SIZE(msg);
+    const size_t headerSize = google::protobuf::io::CodedOutputStream::VarintSize32(messageSize);
+    Data data(headerSize + messageSize);
+    google::protobuf::uint8* buffer = reinterpret_cast<google::protobuf::uint8*>(&*data.begin());
+    google::protobuf::io::CodedOutputStream::WriteVarint32ToArray(messageSize, buffer);
+    msg.SerializeWithCachedSizesToArray(buffer + headerSize);
+
+    return std::make_shared<Data>(data);
+
 }
 
 /*!
@@ -38,6 +48,35 @@ template <typename Message>
 std::shared_ptr<Message> parseDelimited(const void* data, size_t size,
                                         size_t* bytesConsumed = 0)
 {
+    if (data!=0 && size!=0){
+        auto ConstData=(const Data*)data;
+        Data data1(ConstData->begin(),ConstData->end());
+        uint32_t messageSize = 0;
+        size_t headerSize = 0;
+        std::shared_ptr<Message> res = std::make_shared<Message>(Message());
+        google::protobuf::uint8* buffer = reinterpret_cast<google::protobuf::uint8*>(&*(data1.begin()));
+        google::protobuf::io::CodedInputStream stream(buffer,size);
+        std::string str;
+
+        if (stream.ReadVarint32(&messageSize)){
+            headerSize=google::protobuf::io::CodedOutputStream::VarintSize32(messageSize);
+        }else{
+            messageSize=0;
+        }
+
+        if (stream.ReadString(&str, messageSize) && res->ParseFromString(str))
+        {
+            if (bytesConsumed)
+            {
+                *bytesConsumed = headerSize + messageSize;
+            }
+            return res;
+        }
+    }
+    if (bytesConsumed)
+    {
+        *bytesConsumed = 0;
+    }
     return nullptr;
 }
 
